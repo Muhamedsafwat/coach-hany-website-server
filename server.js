57ea39cae7d2f4c9b31fbdea72c977eec8b90cec
muhamedsafwat <musafwat666@gmail.com>
2023-08-14 12:09:30 +0300

    edit middleware

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
 );
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
+app.all("*", function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "https://team-eldon.vercel.app");
   // Request methods you wish to allow
